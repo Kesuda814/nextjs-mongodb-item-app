31751b13063e65fe74c5024965b91047e67b0232
Kesuda814 <u6736640@au.edu>
2026-08-31 10:33:14 +0700

    Completed soft deletion implementation

---
+++ src/lib/cors.js
@@ -0,0 +1,8 @@
+// src/lib/cors.js
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
+export default corsHeaders;
